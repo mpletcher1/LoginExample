63a08ce5a67171a1e299fa1f3c82aaedc5847dca
Matt Pletcher <matt.pletcher@yahoo.com>
2023-08-29 18:36:55 -0400

    updated

--- BulkyBookWeb/Controllers/LoginController.cs
+++ BulkyBookWeb/Controllers/LoginController.cs
@@ -6,12 +6,24 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using BulkyBookWeb.Extensions;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.Hosting;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace BulkyBookWeb.Controllers
 {
     public class LoginController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        //static HttpClient client = new HttpClient();
+
+        public class UserInfo
+        {
+            public string UserName { get; set; }
+            public string code { get; set; }
+        }
 
         public LoginController(ApplicationDbContext db)
         {
@@ -20,8 +32,7 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _db.Categories;
-            return View(objCategoryList);
+            return View();
         }
 
         //GET
@@ -32,10 +43,16 @@ namespace BulkyBookWeb.Controllers
 
         public ActionResult RedirectToAnotherPage()
         {
-            string CLIENT_ID = "7777";
             var privKeyPath = @"C:\Users\mattp\Dropbox\PC\Documents\private.pem";
             var privKey = System.IO.File.ReadAllText(privKeyPath);
-
+            var tokenStringPlain = "acr_values=http%3A%2F%2Fidmanagement.gov%2Fns%2Fassurance%2Fial%2F1&" +
+                        "client_id=urn%3Agov%3Agsa%3Aopenidconnect.profiles%3Asp%3Asso%3Adept_state%3Apassportwizard&" +
+                        "nonce=hithereIamnoncenoncenoncenoncenonce&" +
+                        "prompt=select_account&" +
+                        "redirect_uri=https%3A%2F%2Flocalhost%3A44313%2FLogin&" +
+                        "response_type=code&" +
+                        "scope=openid+email&" +
+                        "state=abcdefghijklmnopabcdefghijklmnop";
             // Create a JWT token with a sample payload
             var tokenHandler = new JwtSecurityTokenHandler();
             var rsaPrivKey = new RSACryptoServiceProvider();
@@ -46,26 +63,85 @@ namespace BulkyBookWeb.Controllers
                 Subject = new ClaimsIdentity(new[]
                 {
                     new Claim("data",
-                        "acr_values=http://idmanagement.gov/ns/assurance/ial/1&\r\n  " +
-                        "client_id=7777&\r\n  " +
-                        "nonce=hithereIamnoncenoncenoncenoncenonce&\r\n  " +
-                        "prompt=select_account&\r\n  " +
-                        "redirect_uri=https://localhost:44313/&\r\n  " +
-                        "response_type=code&\r\n  " +
-                        "scope=openid+email&\r\n  " +
+                        "acr_values=http%3A%2F%2Fidmanagement.gov%2Fns%2Fassurance%2Fial%2F1&" +
+                        "client_id=urn%3Agov%3Agsa%3Aopenidconnect.profiles%3Asp%3Asso%3Adept_state%3Apassportwizard&" +
+                        "nonce=hithereIamnoncenoncenoncenoncenonce&" +
+                        "prompt=select_account&" +
+                        "redirect_uri=https%3A%2F%2Flocalhost%3A44313%2FLogin&" +
+                        "response_type=code&" +
+                        "scope=openid+email&" +
                         "state=abcdefghijklmnopabcdefghijklmnop") // Replace with your payload data
                 }),
                 Expires = DateTime.UtcNow.AddMinutes(10), // Token expiration time
                 SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsaPrivKey), SecurityAlgorithms.RsaSha256Signature)
             };
+
+            /*
+            expect(WebMock).to have_requested(:post, "#{root_url}/oauth/authenticate").
+                with(
+                  body: hash_including(
+                    {
+                        'username' => username,
+                      'password' => password,
+                      'grant_type' => 'implicit',
+                      'response_type' => 'token',
+                      'client_id' => client_id,
+                      'scope' => 'ivs.ippaas.apis',
+                    },
+                  ),
+                  headers:
+                    {
+                        'Content-Type': 'application/json; charset=utf-8',
+                  },
+                )
+            end
+            */
+            Console.WriteLine(tokenDescriptor.ToString());
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
-
+            //var urik = Request.HttpContext.Request.GetDisplayUrl();
             // Redirect to the destination page with the token as a query parameter
-            return Redirect("https://idp.int.identitysandbox.gov/openid_connect/authorize?/" + tokenString);
+            //Response.BufferOutput = true;
+            //Response.Redirect("https://idp.int.identitysandbox.gov/openid_connect/authorize?" + tokenStringPlain);
+            //ViewBag.ReturnURL = "https://idp.int.identitysandbox.gov/openid_connect/authorize?" + tokenStringPlain;
+            return Redirect("https://idp.int.identitysandbox.gov/openid_connect/authorize?" + tokenStringPlain);
+            //response.ToString();
+            
+           // Response.OnCompleted(getResponseInfo());
+            //return Response.;
             //return Json(new { Token = tokenString });
         }
 
+        /*
+
+        static async Task<Uri> RedirectLoginPageAsync(string uri, string jwtInfo)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/products", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<UserInfo> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        
+        public Func<string,Task> getResponseInfo()
+        {
+            return 
+        }
+
+        */
 
 
     }
